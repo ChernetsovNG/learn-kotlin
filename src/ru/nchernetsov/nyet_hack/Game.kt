307c18ab7671541928abb40461c8e0b394e15cf5
ChernetsovNG <n.chernetsov86@gmail.com>
2020-03-09 17:19:16 +0300

    Глава 4

--- src/ru/nchernetsov/nyet_hack/Game.kt
+++ src/ru/nchernetsov/nyet_hack/Game.kt
@@ -15,10 +15,16 @@ fun main() {
         else -> "unknown"
     }
 
-    val auraVisible = isBlessed && healthPoints > 50 || isImmortal
-    val auraColor = if (auraVisible) "GREEN" else "NONE"
+    val auraColor = auraColor(isBlessed, healthPoints, isImmortal)
+    val healthStatus = formatHealthStatus(healthPoints, isBlessed)
 
-    val healthStatus = when (healthPoints) {
+    printPlayerStatus(auraColor, isBlessed, name, healthStatus)
+
+    castFireball()
+}
+
+private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean) =
+    when (healthPoints) {
         100 -> "is in excellent condition!"
         in 90..99 -> "has a few scratches"
         in 75..89 -> if (isBlessed) {
@@ -30,6 +36,18 @@ fun main() {
         else -> "is in awful condition!"
     }
 
+private fun printPlayerStatus(
+    auraColor: String,
+    isBlessed: Boolean,
+    name: String,
+    healthStatus: String
+) {
     println("(Aura: $auraColor) (Blessed: ${if (isBlessed) "YES" else "NO"})")
     println("$name $healthStatus")
 }
+
+private fun auraColor(isBlessed: Boolean, healthPoints: Int, isImmortal: Boolean) =
+    if (isBlessed && healthPoints > 50 || isImmortal) "GREEN" else "NONE"
+
+private fun castFireball(numFireballs: Int = 2) =
+    println("A glass of Fireball springs into existence. (x$numFireballs)")
